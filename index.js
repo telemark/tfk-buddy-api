e80e879ab0c58fed8f59224302794ec5cc5eba7b
maccyber <jonas.enge@gmail.com>
2015-09-03 11:04:37 +0200

    genesis commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var sql = require('mssql');
 var config = require('./config');
 
@@ -5,9 +7,11 @@ var connection = new sql.Connection(config, function(err) {
     // ... error checks
 
     // Query
+    var query = 'SELECT TOP 1000 [ID] ,[BadUsernamePart][ReplaceWith]' +
+      'FROM [dbMetakatalog].[dbo].[tblBadUsernames]';
 
-    var request = new sql.Request(connection); // or: var request = connection.request();
-    request.query('SELECT TOP 1000 [ID] ,[BadUsernamePart],[ReplaceWith] FROM [dbMetakatalog].[dbo].[tblBadUsernames]', function(err, recordset) {
+    var request = new sql.Request(connection); 
+    request.query(query, function(err, recordset) {
         // ... error checks
         console.dir(recordset);
     });
