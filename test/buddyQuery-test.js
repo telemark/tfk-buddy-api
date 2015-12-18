06e9660c9091e928d8cd10e61cbe62d6271ed4cd
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-12-18 07:17:31 +0100

    Adds test for buddyQuery

--- test/buddyQuery-test.js
+++ test/buddyQuery-test.js
@@ -3,3 +3,11 @@
 var tap = require('tap')
 var buddyQuery = require('../lib/buddyQuery')
 
+tap.test('It requires a query.', function (test) {
+  var query = false
+  var expectedErrorMessage = 'Missing required input: query'
+  buddyQuery(query, function (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
+  })
+})
